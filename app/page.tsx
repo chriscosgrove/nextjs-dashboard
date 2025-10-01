fbd041ebafae9d9c83c84271c343b00c63c8404d
Christopher Cosgrove <CHRISTOPHER252@bailliegifford.com>
2025-10-01 17:37:08 +0100

    feat: add testing header to main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { lusitana } from './ui/fonts/fonts';
 import Image from 'next/image';
 
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -48,6 +49,8 @@ export default function Page() {
           />
         </div>
       </div>
+      <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <h1>testing</h1></div>
     </main>
   );
 }
